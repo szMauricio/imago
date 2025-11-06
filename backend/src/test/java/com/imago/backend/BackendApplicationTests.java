1ad279bb1b31e66dfb5887f8c33aacfd424eac34
szMauricio <szmauricio02@gmail.com>
2025-11-06 10:49:21 -0300

    Initial configuration

---
+++ backend/src/test/java/com/imago/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imago.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
